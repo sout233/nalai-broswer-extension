6b7eccdcb552f089e8a9c00ac283a359f343b28d
sout_Nantang <55623894+sout233@users.noreply.github.com>
2024-12-05 12:39:34 +0800

    🔧 build: tailwindcss

---
+++ src/contents/all.ts
@@ -0,0 +1,43 @@
+// import type { PlasmoCSConfig } from "plasmo"
+
+// export const config: PlasmoCSConfig = {
+//     matches: ["https://*", "http://*"]
+// }
+
+// window.addEventListener("load", () => {
+//     console.log("content script loaded")
+
+//     document.body.style.background = "pink"
+// })
+
+
+// chrome.downloads.onCreated.addListener(async (downloadItem) => {
+//     console.log("Download attempt blocked:", downloadItem.url);
+
+//     const ua = navigator.userAgent;
+
+//     const data = {
+//         browser: "Firefox",
+//         userAgent: ua,
+//         downloadUrl: downloadItem.url
+//     };
+
+//     // 发送POST请求到服务器
+//     fetch("https://127.0.0.1:10389/download", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(data)
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log("Success:", data);
+//         })
+//         .catch((error) => {
+//             console.error("Error:", error);
+//         });
+
+//     // 取消下载
+//     chrome.downloads.cancel(downloadItem.id);
+// });
