d85a36ae4731835bc3988facdb5337f4ba9ef74d
sout_Nantang <55623894+sout233@users.noreply.github.com>
2024-12-19 12:39:09 +0800

    ✨ feat: improve popup ui

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,36 +1,49 @@
-import {
-  Button,
-  FluentProvider,
-  Label,
-  Switch,
-  Text,
-  Title2,
-  Title3,
-  webLightTheme
-} from "@fluentui/react-components"
-import React, { useState } from "react"
+import React, { useState } from 'react';
+import { FluentProvider, webLightTheme, TabList, Tab, Switch, Button } from '@fluentui/react-components';
 
 import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [selectedTab, setSelectedTab] = useState("this-page-tab");
+
+  const handleTabChange = (event, data) => {
+    setSelectedTab(data.value);
+  };
+
+  const renderSwitches = () => (
+    <>
+      <div className="flex justify-between w-full">
+        <Switch labelPosition="before" label="下载截获" className="flex-grow" />
+      </div>
+      <div className="flex justify-between w-full">
+        <Switch labelPosition="before" label="资源嗅探" className="flex-grow" />
+      </div>
+    </>
+  );
 
   return (
     <FluentProvider
       theme={webLightTheme}
       className="popup-container w-96 p-4 rounded-lg">
-      <div className="flex flex-col">
-        <Label size="large">设置（仅针对此页面）</Label>
+      <TabList selectedValue={selectedTab} onTabSelect={handleTabChange}>
+        <Tab value="this-page-tab">此页面</Tab>
+        <Tab value="global-tab">全局</Tab>
+      </TabList>
+
+      {selectedTab === "this-page-tab" && (
         <div className="flex flex-col">
-          <Switch labelPosition="before" label="下载截获" />
-          <Switch labelPosition="before" label="资源嗅探" />
+          {renderSwitches()}
         </div>
-      </div>
-      <div className="flex flex-col">
-        <Button appearance="primary">Open Settings</Button>
-      </div>
+      )}
+
+      {selectedTab === "global-tab" && (
+        <div className="flex flex-col">
+          {renderSwitches()}
+          <Button appearance="primary">Open Settings</Button>
+        </div>
+      )}
     </FluentProvider>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
