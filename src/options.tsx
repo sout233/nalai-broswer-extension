74470a1597c134dbbe6a3fd012349fbbd9e8576b
sout233 <2273466101@qq.com>
2024-12-20 20:32:17 +0800

    ✨ feat: update options page

--- src/options.tsx
+++ src/options.tsx
@@ -1,22 +1,48 @@
-import React, { useReducer } from "react"
-import { useState } from "react"
+import React, { useReducer, useState } from "react"
+
 import "./style.css"
+
+import {
+  FluentProvider,
+  Tab,
+  TabList,
+  Title3,
+  webLightTheme
+} from "@fluentui/react-components"
 
 function IndexOptions() {
   const [data, setData] = useState("")
   const [count, increase] = useReducer((c) => c + 1, 0)
 
-  return (
-    <button
-      onClick={() => increase()}
-      type="button"
-      className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-      Count:
-      <span className="inline-flex items-center justify-center w-8 h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
-        {count}
-      </span>
-    </button>
-  )
+  const [selectedTab, setSelectedTab] = useState("general")
+
+  const handleTabChange = (
+    _event: any,
+    data: { value: React.SetStateAction<string> }
+  ) => {
+    setSelectedTab(data.value)
+  }
+
+return (
+  <FluentProvider theme={webLightTheme}>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="max-w-xl max-h-[100rem] justify-center shadow-lg rounded-lg mx-auto">
+        <div className="flex flex-col">
+          <Title3 className="mb-2 ml-2">Nalai</Title3>
+          <TabList
+            vertical={true}
+            selectedValue={selectedTab}
+            onTabSelect={handleTabChange}>
+            <Tab value={"general"}>General</Tab>
+            <Tab value={"appearance"}>Appearance</Tab>
+            <Tab value={"download"}>下载</Tab>
+            <Tab value={"capture"}>嗅探</Tab>
+          </TabList>
+        </div>
+      </div>
+    </div>
+  </FluentProvider>
+)
 }
 
 export default IndexOptions
